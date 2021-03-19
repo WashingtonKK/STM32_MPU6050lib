12f9907be799ea509ed50302df471ed6de16ad7e
Washington Kamadi Kigani <washingtonkigan@gmail.com>
2021-03-19 21:04:51 +0300

    Modifying to fit my use case

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lC:/Users/WASHINGTON/Documents/GitHub/STM32_MPU6050lib/MPU6050.h
+
